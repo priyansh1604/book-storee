e9c34c5176b4c818c77e3f482b3291b39fcd5e5a
priyansh1604 <101087674+priyansh1604@users.noreply.github.com>
2023-11-24 13:28:17 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cors());
 app.use("/books", router);
 
-const uri = "mongodb+srv://ansh:KRQWJleVjfNZqZYX@cluster0.pn7oy3b.mongodb.net/?retryWrites=true&w=majority";
+const uri = "";
 
 mongoose.connect(uri)
 .then(()=>{
